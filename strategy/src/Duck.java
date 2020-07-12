adc0d1be9ded0ef017991e9f047035cd1017301e
王志坤 <wzk@disroot.org>
2020-07-12 18:44:14 +0800

    strategy

---
+++ strategy/src/Duck.java
@@ -0,0 +1,30 @@
+public abstract class Duck {
+
+    public Duck() {
+    }
+
+    FlyBehavior flyBehavior;
+    QuackBehavior quackBehavior;
+
+    public abstract  void display();
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+
+    public void setFlyBehavior(FlyBehavior flyBehavior) {
+        this.flyBehavior = flyBehavior;
+    }
+
+    public void setQuackBehavior(QuackBehavior quackBehavior) {
+        this.quackBehavior = quackBehavior;
+    }
+
+    public void swim() {
+        System.out.println("All ducks float, even decoys!");
+    }
+}
