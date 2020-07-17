92d630693c45c8325ab4f27be86784436a9489eb
王志坤 <wzk@disroot.org>
2020-07-17 20:46:43 +0800

    factory

---
+++ factory/src/ChicagoStylePizzaStore.java
@@ -0,0 +1,8 @@
+public class ChicagoStylePizzaStore extends PizzaStore {
+    @Override
+    protected Pizza createPizza(String type) {
+        if (type.equals("cheese")) {
+            return new ChicagoStyleCheesePizza();
+        } else return null;
+    }
+}
---
+++ factory/src/NYPizzaStore.java
@@ -0,0 +1,8 @@
+public class NYPizzaStore extends PizzaStore {
+    @Override
+    protected Pizza createPizza(String type) {
+        if(type.equals("cheese")) {
+            return new NYStyleCheesePizza();
+        } else return null;
+    }
+}
