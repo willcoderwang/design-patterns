b4285984bfb1a7afe2fb26bee25a1d628c89ef57
王志坤 <wzk@disroot.org>
2020-07-20 19:42:01 +0800

    template method

---
+++ template-method/src/BeverageTestDrive.java
@@ -0,0 +1,13 @@
+public class BeverageTestDrive {
+    public static void main(String[] args) {
+
+        Tea tea = new Tea();
+        Coffee coffee = new Coffee();
+
+        System.out.println("Making tea...");
+        tea.prepareRecipe();
+
+        System.out.println("Making coffee...");
+        coffee.prepareRecipe();
+    }
+}
